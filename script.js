8ac5032c021721662c9c34254b9d61899683728a
Baxtzodjon <rasulovbaxtzod@gmailcom>
2023-12-14 14:10:09 +0500

    added counting javascript

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let cont = document.querySelector('.container')
 let btn_five = document.querySelector('[data-five]')
 let btn_all = document.querySelector('[data-all]')
 let total_view = document.querySelector('#total')
+let cartTab_div = document.createElement('div')
+let open_Cart = document.querySelector('[data-cart]')
+let closeCart = document.querySelector('.close_times2')
+let body = document.querySelector('body')
+
 let saveds = []
 
 btn_five.onclick = () => {
@@ -13,6 +18,7 @@ btn_all.onclick = () => {
 }
 
 reload(arr)
+reload_cart(saveds)
 
 function reload(product) {
     cont.innerHTML = ""
@@ -31,13 +37,18 @@ function reload(product) {
         div_item.classList.add('item')
         descrip_div.classList.add('description')
 
-        img.src = /* item.image */
-            img.alt = item.title
+        img.src = item.image
+        img.alt = item.title
 
         h2.innerHTML = `${item.category} (${item.rating.count})`
         p.innerHTML = item.description.slice(0, 100)
         b.innerHTML = " read more..."
         button.innerHTML = "Save"
+
+        if (saveds.includes(item.id)) {
+            button.innerHTML = "Добавлен в корзину"
+            button.classList.add('active-btn')
+        }
 
         // append
         div_item.append(img, descrip_div)
@@ -48,14 +59,14 @@ function reload(product) {
         // functions
         button.onclick = () => {
             if (saveds.includes(item.id)) {
-                // delete
+                //deleteBtn
                 let idx = saveds.indexOf(item.id)
                 saveds.splice(idx, 1)
                 button.classList.remove('active-btn')
                 button.innerHTML = 'Save'
             } else {
                 button.classList.add('active-btn')
-                button.innerHTML = 'Добавлен в избранное'
+                button.innerHTML = 'Добавлен в корзину'
                 saveds.push(item.id)
             }
             total_view.innerHTML = saveds.length
@@ -64,7 +75,7 @@ function reload(product) {
         b.onclick = () => {
             if (b.innerHTML === "close") {
                 p.innerHTML = item.description.slice(0, 100)
-                b.innerHTML = " read more..."
+                b.innerHTML = "read more..."
             } else {
                 p.innerHTML = item.description
                 b.innerHTML = "close"
@@ -74,10 +85,6 @@ function reload(product) {
     }
 }
 
-let open_Cart = document.querySelector('[data-cart]')
-let closeCart = document.querySelector('.close_times2')
-let body = document.querySelector('body')
-
 open_Cart.onclick = () => {
     body.classList.toggle('showCart')
 }
@@ -86,19 +93,30 @@ closeCart.onclick = () => {
     body.classList.toggle('showCart')
 }
 
-body.onclick = function (e) {
-    if (e.target.className != 'delete') return
-    let cartTab_bellissimo_pizza = e.target.closest('.cartTab_bellissimo_pizza')
-    cartTab_bellissimo_pizza.remove()
-}
 
-let cartTab_div = document.createElement('div')
+// create 
+let counting_price = document.createElement('div')
+let total_price = document.createElement('p')
+let cost = document.createElement('p')
 
-function reload_cart(reload) {
+// styling
+counting_price.classList.add('couting_number')
+total_price.classList.add('total_price')
+cost.classList.add('cost')
+
+total_price.innerHTML = "Итого:"
+cost.innerHTML = "0 сум"
+
+
+
+function reload_cart(ids) {
     cartTab_div.innerHTML = ""
 
-    for (let item of reload) {
-        let cartItem = arr.find(el => el.id === item)
+    let total = 0
+
+    for (let id of ids) {
+        let cartItem = arr.find(el => el.id === id)
+        total = +(total + cartItem.price).toFixed(2)
 
         // create
         let cart_flex_bellissimo_pizza_div = document.createElement('div')
@@ -109,81 +127,86 @@ function reload_cart(reload) {
         let p_text = document.createElement('p')
         let p_text_type_of_pizza = document.createElement('p')
         let counter_bellissimo_pizza_div = document.createElement('div')
-        let minus_bellissimo_pizza_div = document.createElement('div')
-        let digit_bellissimo_pizza_div = document.createElement('div')
-        let plus_bellissimo_pizza_div = document.createElement('div')
-        let price_bellissimo_pizza_p = document.createElement('p')
+        let minus = document.createElement('div')
+        let count = document.createElement('div')
+        let plus = document.createElement('div')
+        let price = document.createElement('p')
         let real_price_p = document.createElement('p')
         let trashIcon2_div = document.createElement('div')
         let trashIcon2_img = document.createElement('img')
-        let p_trashIcon2_delete = document.createElement('delete')
+        let deleteBtn = document.createElement('delete')
 
         // styling
         cartTab_div.classList.add('cartTab')
         cart_flex_bellissimo_pizza_div.classList.add('cart_flex_bellissimo_pizza')
         btn_close_items2.classList.add('close_times2')
-        btn_close_items2.innerHTML = "X"
         cartTab_bellissimo_pizza_div.classList.add('cartTab_bellissimo_pizza')
         box_block_bellissimo_pizza_div.classList.add('box_block_bellissimo_pizza')
         img_photo.classList.add('img_pizza')
+        p_text.classList.add('judge_pizza')
+        p_text_type_of_pizza.classList.add('type_of_pizza')
+        counter_bellissimo_pizza_div.classList.add('counter_bellissimo_pizza')
+        minus.classList.add('minus_bellissimo_pizza')
+        count.classList.add('digit_bellissimo_pizza')
+        plus.classList.add('plus_bellissimo_pizza')
+        price.classList.add('price_bellissimo_pizza')
+        real_price_p.classList.add('real_price')
+        trashIcon2_div.classList.add('trashIcon2')
+        deleteBtn.classList.add('delete')
+
         img_photo.setAttribute('src', cartItem.image)
         img_photo.setAttribute('alt', 'images')
-        p_text.classList.add('judge_pizza')
-        p_text.innerHTML = cartItem.title
-        p_text_type_of_pizza.classList.add('type_of_pizza')
-        p_text_type_of_pizza.innerHTML = cartItem.category
-        counter_bellissimo_pizza_div.classList.add('counter_bellissimo_pizza')
-        minus_bellissimo_pizza_div.classList.add('minus_bellissimo_pizza')
-        minus_bellissimo_pizza_div.innerHTML = "-"
-        digit_bellissimo_pizza_div.classList.add('digit_bellissimo_pizza')
-        /* digit_bellissimo_pizza_div.classList.add('#digit') */
-        digit_bellissimo_pizza_div.innerHTML = "1"
-        plus_bellissimo_pizza_div.classList.add('plus_bellissimo_pizza')
-        plus_bellissimo_pizza_div.innerHTML = "+"
-        price_bellissimo_pizza_p.classList.add('price_bellissimo_pizza')
-        /* price_bellissimo_pizza_p.classList.add('#price') */
-        price_bellissimo_pizza_p.innerHTML = cartItem.price
-        real_price_p.classList.add('real_price')
-        // real_price_p.innerHTML = "60 000 so'm"
-        trashIcon2_div.classList.add('trashIcon2')
         trashIcon2_img.setAttribute('src', './img/trash_gif_icon.gif')
         trashIcon2_img.setAttribute('alt', 'кнопка удаления')
-        p_trashIcon2_delete.classList.add('delete')
-        p_trashIcon2_delete.innerHTML = "Удалить"
+        count.id = 'digit' // добавлять id
+
+        btn_close_items2.innerHTML = "X"
+        p_text.innerHTML = cartItem.title
+        p_text_type_of_pizza.innerHTML = cartItem.category
+        minus.innerHTML = "-"
+        count.innerHTML = "1"
+        plus.innerHTML = "+"
+        price.innerHTML = cartItem.price
+        deleteBtn.innerHTML = "Удалить"
+        // digit.classList.add('#digit')
+        // digit.setAttribute('id', 213213) // создать id
+        // digit.setAttribute('id', 'digit') // добавлять id второй способ
+        /* price.classList.add('#price') */
+        // real_price_p.innerHTML = "60 000 so'm"
 
         // append 
         body.append(cartTab_div)
-        cartTab_div.append(/* cart_flex_bellissimo_pizza_div, */ cartTab_bellissimo_pizza_div)
-        // cart_flex_bellissimo_pizza_div.append(btn_close_items2)
+        cartTab_div.append(/* cart_flex_bellissimo_pizza_div,  */cartTab_bellissimo_pizza_div, counting_price)
+        counting_price.append(total_price, cost)
+        /* cart_flex_bellissimo_pizza_div.append(btn_close_items2) */
         cartTab_bellissimo_pizza_div.append(box_block_bellissimo_pizza_div)
-        box_block_bellissimo_pizza_div.append(img_photo, p_text, p_text_type_of_pizza, counter_bellissimo_pizza_div, price_bellissimo_pizza_p, real_price_p)
-        counter_bellissimo_pizza_div.append(minus_bellissimo_pizza_div, digit_bellissimo_pizza_div, plus_bellissimo_pizza_div, trashIcon2_div)
-        trashIcon2_div.append(trashIcon2_img, p_trashIcon2_delete)
+        box_block_bellissimo_pizza_div.append(img_photo, p_text, p_text_type_of_pizza, counter_bellissimo_pizza_div, price, real_price_p)
+        counter_bellissimo_pizza_div.append(minus, count, plus, trashIcon2_div)
+        trashIcon2_div.append(trashIcon2_img, deleteBtn)
 
-        let count = 1;
-        let price = cartItem.price;
-
-        function plus() {
-            count++;
-            price *= 2;
-            updateDisplay();
-        }
-
-        function minus() {
-            if (count > 1) {
-                count--;
-                price /= 2;
-                updateDisplay();
+        // functions
+        plus.onclick = () => {
+            if (count.innerHTML < cartItem.rating.count) {
+                count.innerHTML++
+                price.innerHTML = (cartItem.price * count.innerHTML).toFixed(2)
             }
-        }
+            total = +(total + cartItem.price).toFixed(2)
 
-        function updateDisplay() {
-            document.querySelector(".digit_bellissimo_pizza").innerHTML = count;
-            document.querySelector(".price_bellissimo_pizza").innerHTML = price;
+            console.log({ total });
         }
-
-        document.querySelector(".minus_bellissimo_pizza").addEventListener("click", minus);
-        document.querySelector(".plus_bellissimo_pizza").addEventListener("click", plus);
+        minus.onclick = () => {
+            if (count.innerHTML > 0) {
+                count.innerHTML--
+                price.innerHTML = (cartItem.price * count.innerHTML).toFixed(2)
+                total = +(total - cartItem.price).toFixed(2)
+            }
+            console.log(total);
+        }
+        deleteBtn.onclick = () => {
+            let idx = saveds.indexOf(cartItem.id)
+            saveds.splice(idx, 1)
+            cartTab_bellissimo_pizza_div.remove()
+            reload(arr)
+        }
     }
 }
-
